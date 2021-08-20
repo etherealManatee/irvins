90c1d4ef5eca473d921594b477ccb90fc4c1b256
hizashi <zytwp@hotmail.com>
2021-08-20 11:39:07 +0800

    backend done

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router()
-const Product = require('../models/product.model')
+const ProductModel = require('../models/product.model')
 
 //adding new product
 router.post("/products", async (req, res) => {
     try {
-        console.log("h", req.body)
-        let product = new Product(req.body)
+        // console.log("h", req.body)
+        let product = new ProductModel(req.body)
         await product.save().catch(err => console.log(err))
 
-        res.status(201).json({})
+        res.status(201).json({data: product})
     } catch (e) {
         res.status(400).json({"message":"error creating product"})
     }
@@ -17,10 +17,38 @@ router.post("/products", async (req, res) => {
 //view one product
 router.get("/products/:id", async (req,res) => {
     try{
-        let product = await ProductModel
-    } catch (e) {
+        let product = await ProductModel.findById(req.params.id)
 
+        res.status(200).json({data: product})
+    } catch (e) {
+        res.status(400).json({"message":"error in request"})
     }
 })
+
+//edit one product
+router.put("/products/:id", async (req,res) => {
+    try{
+        let product = await ProductModel.findByIdAndUpdate(req.params.id, req.body)
+
+        let updatedProduct = await ProductModel.findById(req.params.id)
+
+        res.status(200).json({data: updatedProduct})
+    } catch (e) {
+        res.status(400).json({"message":"error in request"})
+    }
+})
+
+//delete on product
+router.delete("/products/:id", async (req,res) => {
+    try{
+        let product = await ProductModel.findByIdAndDelete(req.params.id)
+
+        res.status(200).json({data: [{id: product._id}]})
+    } catch (e) {
+        res.status(400).json({"message":"error in request"})
+    }
+})
+
+
 
 module.exports = router
